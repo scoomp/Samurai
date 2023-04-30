3c9e4997daaa326ee3babb9dc45a913ece68fd32
scoomp <moshnyakov.alexei@yandex.ru>
2023-04-30 15:13:57 +0300

    2 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from "./Compnents/Header/Header";
+import Navbar from "./Compnents/Navbar/Navbar";
+import Profile from "./Compnents/Profile/Profile";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+export function App() {
+
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <Profile/>
+
+        </div>
+    );
 }
 
-export default App;
+
+
